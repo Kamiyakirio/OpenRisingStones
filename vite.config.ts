558559cd7a4eac99c73440bbe1a3ecacb50cfb0f
Kamiyakirio <kamikomi255@gmail.com>
2026-08-22 17:07:43 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  // Keep Vite's development server stable for Tauri's native window.
+  clearScreen: false,
+  server: {
+    host: process.env.TAURI_DEV_HOST || false,
+    port: 1420,
+    strictPort: true,
+  },
+})
